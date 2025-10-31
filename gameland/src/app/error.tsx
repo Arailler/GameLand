b7c1bd1252c712924c968b852399f568b0c2eef9
Arailler <44024305+Arailler@users.noreply.github.com>
2025-10-31 20:18:11 +0100

    fix: make 500 error page work (#41)

--- gameland/src/app/error.tsx
+++ gameland/src/app/error.tsx
@@ -16,13 +16,14 @@ export default function ServerError() {
         <Image
           src="https://media.giphy.com/media/3o6ZsV7wEpihIs2Xwc/giphy.gif"
           alt="Funny server error meme"
+          unoptimized
           fill
           className="object-contain rounded-lg shadow-lg"
         />
       </div>
       <Link
         href="/"
-        className="px-6 py-3 bg-red-600 text-white rounded-lg shadow hover:bg-red-700 transition"
+        className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition"
       >
         Go back home
       </Link>
