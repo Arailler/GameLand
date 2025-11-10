61b4ba066135a46d34e19560e3635e067e1c6589
Arailler <maouchi76@hotmail.fr>
2025-11-10 14:46:17 +0100

    chore: make the aws load balancer controller scenario work

--- infrastructure/terraform/environments/k8s/outputs.tf
+++ infrastructure/terraform/environments/k8s/outputs.tf
@@ -1,3 +1,7 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
 output "bastion_public_ips" {
   value = module.bastion_ec2.public_ips
 }
--- infrastructure/terraform/modules/ec2_instance/main.tf
+++ infrastructure/terraform/modules/ec2_instance/main.tf
@@ -5,6 +5,12 @@ resource "aws_instance" "this" {
   subnet_id              = element(var.subnet_ids, count.index % length(var.subnet_ids))
   vpc_security_group_ids = var.security_group_ids
   key_name               = var.key_name
+  iam_instance_profile   = var.iam_instance_profile
+   metadata_options {
+    http_tokens                 = "required"
+    http_endpoint               = "enabled"
+    http_put_response_hop_limit = 2
+  }
   tags = merge(
     var.tags, { Name = "${var.name_prefix}-${count.index + 1}", Terraform = "true" }
   )
